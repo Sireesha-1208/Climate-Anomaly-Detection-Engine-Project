674348672f102aa379e8dc37f13c12dca0829ca0
Sireesha <chinnarasanagarisireeshareddy@gmail.com>
2026-06-11 23:19:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("🌍 Climate Anomaly Detection Engine")
+st.write("Detect unusual weather patterns compared to historical averages")
+
+st.success("App is working successfully!")
